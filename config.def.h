beef59735b4f15fdebf30107cc1ca16d3b431a56
MahouShoujoMivutilde <MahouShoujoMivutilde@users.noreply.github.com>
2019-09-03 22:21:53 +0300

    upd colors

--- config.def.h
+++ config.def.h
@@ -7,14 +7,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 
-static const char norm_fg[] = "#6F8CA0";
+static const char norm_fg[] = "#7eb9e1";
 static const char norm_bg[] = "#0f0d0c";
 static const char sel_fg[] = "#f5ffff";
+static const char sel_bg[] = "#2b2522";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { norm_fg,   norm_bg,   norm_bg},
-	[SchemeSel]  = { sel_fg,    norm_bg,   norm_bg},
+	[SchemeSel]  = { norm_fg,   sel_bg,    norm_bg},
 };
 
 /* tagging */
