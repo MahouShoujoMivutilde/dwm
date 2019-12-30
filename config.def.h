e37ad69bca87e6e36e156aedc14f6c7364f48833
MahouShoujoMivutilde <mivu.sedai@gmail.com>
2019-12-31 01:24:05 +0300

    bring zoomswap bindings back

--- config.def.h
+++ config.def.h
@@ -87,6 +87,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_o,      setcfact,       {.f =  0.00} },
 	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
+	{ MODKEY,                       XK_z,      zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_a,      setlayout,      {.v = &layouts[0]} },
